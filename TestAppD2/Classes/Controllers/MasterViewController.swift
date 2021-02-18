3e45c34f5504a8cebd5b5dfca1260a97c21fc393
Sergei Popyvanov <s.popyvanov@gmail.com>
2021-02-18 18:23:22 +0400

    Расставлены марки, сгруппированы и расставлены уровни доступов для методов и свойств в MasterViewController

--- TestAppD2/Classes/Controllers/MasterViewController.swift
+++ TestAppD2/Classes/Controllers/MasterViewController.swift
@@ -8,37 +8,51 @@
 
 import UIKit
 
-class MasterViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+/// Контроллер главного экрана
+final class MasterViewController: UIViewController {
     
-    let kCellIdentifier = "CellForQuestion"
-    @IBOutlet var tableView: UITableView!
-    var activityIndicatorView: UIActivityIndicatorView!
-    var questions: [Item]? = []
-    var refreshControl: UIRefreshControl?
-    var loadMoreStatus = false
-    var numberOfPageToLoad: Int = 0
-    var requestedTag = ""
-    @IBOutlet weak var leadingTabelViewLayoutConstraint: NSLayoutConstraint!
-    var panRecognizer: UIPanGestureRecognizer?
-    var screenEdgePanRecognizer: UIScreenEdgePanGestureRecognizer?
-    @IBOutlet weak var trailingTableViewLayoutConstraint: NSLayoutConstraint!
+    // MARK: - IBOutlet
+    
+    @IBOutlet private var tableView: UITableView!
+    @IBOutlet private var leadingTabelViewLayoutConstraint: NSLayoutConstraint!
+    @IBOutlet private var trailingTableViewLayoutConstraint: NSLayoutConstraint!
+    
+    // MARK: - Private Properties
+    
+    private let kCellIdentifier = "CellForQuestion"
+    private var activityIndicatorView: UIActivityIndicatorView!
+    private var questions: [Item]? = []
+    private var refreshControl: UIRefreshControl?
+    private var loadMoreStatus = false
+    private var numberOfPageToLoad: Int = 0
+    private var requestedTag = ""
+    private var panRecognizer: UIPanGestureRecognizer?
+    private var screenEdgePanRecognizer: UIScreenEdgePanGestureRecognizer?
+    
+    // MARK: - UIViewController
     
     override func viewDidLoad() {
-        tableView.register(UINib(nibName: "QuestionTableViewCell", bundle: nil), forCellReuseIdentifier: kCellIdentifier)
+        tableView.register(
+            UINib(nibName: "QuestionTableViewCell", bundle: nil),
+            forCellReuseIdentifier: kCellIdentifier)
         numberOfPageToLoad = 1
         addRefreshControlOnTabelView()
         settingDynamicHeightForCell()
         addActivityIndicator()
-        NotificationCenter.default.addObserver(self, selector: #selector(self.requestedTagNotification(_:)), name: NSNotification.Name("RequestedTagNotification"), object: nil)
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(self.requestedTagNotification(_:)),
+            name: NSNotification.Name("RequestedTagNotification"), object: nil)
         requestedTag = ArrayOfTags.shared[0]
         definesPresentationContext = true
         questions = [Item]()
-        FabricRequest.request(tagged: requestedTag, numberOfPageToLoad: numberOfPageToLoad) { (data) in
+        FabricRequest.request(
+            tagged: requestedTag,
+            numberOfPageToLoad: numberOfPageToLoad) { (data) in
             self.reload(inTableView: data, removeAllObjects: true)
         }
         numberOfPageToLoad += 1
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let indexPath: IndexPath? = tableView.indexPathForSelectedRow
         let detailViewController = (segue.destination as? UINavigationController)?.topViewController as? DetailViewController
@@ -48,8 +62,42 @@ class MasterViewController: UIViewController, UITableViewDelegate, UITableViewDa
         detailViewController?.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
         detailViewController?.navigationItem.leftItemsSupplementBackButton = true
     }
-
-    func addRefreshControlOnTabelView() {
+    
+    // MARK: - IBAction
+    
+    @IBAction func slideMenu(_ sender: Any) {
+        if leadingTabelViewLayoutConstraint.constant == 0 {
+            leadingTabelViewLayoutConstraint.constant = UIScreen.main.bounds.size.width / 2
+            trailingTableViewLayoutConstraint.constant = UIScreen.main.bounds.size.width * -0.5
+            UIView.animate(
+                withDuration: 0.3,
+                delay: 0.0,
+                options: .layoutSubviews,
+                animations: {
+                    self.view.layoutIfNeeded()
+                })
+            screenEdgePanRecognizer?.isEnabled = false
+            panRecognizer?.isEnabled = true
+            tableView.allowsSelection = false
+        } else {
+            leadingTabelViewLayoutConstraint.constant = 0
+            trailingTableViewLayoutConstraint.constant = 0
+            UIView.animate(
+                withDuration: 0.3,
+                delay: 0.0,
+                options: .layoutSubviews,
+                animations: {
+                self.view.layoutIfNeeded()
+            })
+            screenEdgePanRecognizer?.isEnabled = true
+            panRecognizer?.isEnabled = false
+            tableView.allowsSelection = true
+        }
+    }
+    
+    // MARK: - Private Methods
+    
+    private func addRefreshControlOnTabelView() {
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(self.reloadData), for: .valueChanged)
         if let refreshControl = refreshControl {
@@ -57,23 +105,27 @@ class MasterViewController: UIViewController, UITableViewDelegate, UITableViewDa
         }
     }
     
-    func settingDynamicHeightForCell() {
+    private func settingDynamicHeightForCell() {
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 100
     }
     
-    func addActivityIndicator() {
+    private func addActivityIndicator() {
         activityIndicatorView = UIActivityIndicatorView()
         activityIndicatorView.style = .gray
         let bounds: CGRect = UIScreen.main.bounds
-        activityIndicatorView.center = CGPoint(x: bounds.size.width / 2, y: bounds.size.height / 2)
+        activityIndicatorView.center = CGPoint(
+            x: bounds.size.width / 2,
+            y: bounds.size.height / 2)
         activityIndicatorView.hidesWhenStopped = true
         view.addSubview(activityIndicatorView)
     }
-
-    @objc func reloadData() {
+    
+    @objc private func reloadData() {
         numberOfPageToLoad = 1
-        FabricRequest.request(tagged: requestedTag, numberOfPageToLoad: numberOfPageToLoad) { (data) in
+        FabricRequest.request(
+            tagged: requestedTag,
+            numberOfPageToLoad: numberOfPageToLoad) { (data) in
             self.reload(inTableView: data, removeAllObjects: true)
         }
         numberOfPageToLoad += 1
@@ -88,7 +140,7 @@ class MasterViewController: UIViewController, UITableViewDelegate, UITableViewDa
         }
     }
     
-    func reload(inTableView jsonData: Data?, removeAllObjects: Bool) {
+    private func reload(inTableView jsonData: Data?, removeAllObjects: Bool) {
         if removeAllObjects {
             questions = [Item]()
         }
@@ -100,6 +152,23 @@ class MasterViewController: UIViewController, UITableViewDelegate, UITableViewDa
             self.activityIndicatorView.stopAnimating()
         })
     }
+    
+    // MARK: - Notification
+    
+    @objc private func requestedTagNotification(_ notification: Notification?) {
+        activityIndicatorView.startAnimating()
+        requestedTag = notification?.object as! String
+        numberOfPageToLoad = 1
+        FabricRequest.request(tagged: requestedTag, numberOfPageToLoad: numberOfPageToLoad) { (data) in
+            self.reload(inTableView: data, removeAllObjects: true)
+        }
+        numberOfPageToLoad += 1
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension MasterViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if questions?.count == 0 {
@@ -115,6 +184,11 @@ class MasterViewController: UIViewController, UITableViewDelegate, UITableViewDa
         }
         return cell!
     }
+}
+
+// MARK: - UITableViewDelegate
+
+extension MasterViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "DetailSegue", sender: self)
@@ -129,47 +203,16 @@ class MasterViewController: UIViewController, UITableViewDelegate, UITableViewDa
             activityIndicatorView.center = CGPoint(x: bounds.size.width / 2, y: bounds.size.height - 50)
             activityIndicatorView.startAnimating()
             loadMoreStatus = true
-            FabricRequest.request(tagged: requestedTag, numberOfPageToLoad: numberOfPageToLoad) { (data) in
+            FabricRequest.request(
+                tagged: requestedTag,
+                numberOfPageToLoad: numberOfPageToLoad) { (data) in
                 self.reload(inTableView: data, removeAllObjects: false)
                 self.loadMoreStatus = false
                 self.numberOfPageToLoad += 1
-                self.activityIndicatorView.center = CGPoint(x: bounds.size.width / 2, y: bounds.size.height / 2)
+                self.activityIndicatorView.center = CGPoint(
+                    x: bounds.size.width / 2,
+                    y: bounds.size.height / 2)
             }
         }
     }
-
-    // MARK: - Notification
-    @objc func requestedTagNotification(_ notification: Notification?) {
-        activityIndicatorView.startAnimating()
-        requestedTag = notification?.object as! String
-        numberOfPageToLoad = 1
-        FabricRequest.request(tagged: requestedTag, numberOfPageToLoad: numberOfPageToLoad) { (data) in
-            self.reload(inTableView: data, removeAllObjects: true)
-        }
-        numberOfPageToLoad += 1
-    }
-    
-    // MARK: - IBAction
-    @IBAction func slideMenu(_ sender: Any) {
-        if leadingTabelViewLayoutConstraint.constant == 0 {
-            leadingTabelViewLayoutConstraint.constant = UIScreen.main.bounds.size.width / 2
-            trailingTableViewLayoutConstraint.constant = UIScreen.main.bounds.size.width * -0.5
-            UIView.animate(withDuration: 0.3, delay: 0.0, options: .layoutSubviews, animations: {
-                self.view.layoutIfNeeded()
-            })
-            screenEdgePanRecognizer?.isEnabled = false
-            panRecognizer?.isEnabled = true
-            tableView.allowsSelection = false
-        } else {
-            leadingTabelViewLayoutConstraint.constant = 0
-            trailingTableViewLayoutConstraint.constant = 0
-            UIView.animate(withDuration: 0.3, delay: 0.0, options: .layoutSubviews, animations: {
-                self.view.layoutIfNeeded()
-            })
-            screenEdgePanRecognizer?.isEnabled = true
-            panRecognizer?.isEnabled = false
-            tableView.allowsSelection = true
-        }
-    }
 }
-
